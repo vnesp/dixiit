f5256248fa15a6df67373fa2be01b7caeadfe2eb
Vitaly Nespirny <nespirny.v.n@econri.org>
2023-11-25 19:58:46 +0300

    add make gallery

--- dixit.py
+++ dixit.py
@@ -6,22 +6,54 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = set()
+        self.usedCards = set()
+        self.score = 0
 
-    def __str__(self):
-        return self.name
 
     def putCard(self, card):
         self.hand.add(card)
 
+
     def getCard(self, card):
         if card in self.hand:
             self.hand.remove(card)
+            self.usedCards.add(card)
             return True
         print("Player {} doesn't have card {}".format(self.name, card))
         return False
 
+
+    def takeBackCard(self, card):
+        if card in self.usedCards:
+            self.usedCards.remove(card)
+            self.hand.add(card)
+            return True
+        print("Player {} haven't used card {}".format(self.name, card))
+        return False
+
+
+    def createAssociation(self):
+        description = input('{}, give your assocciaton: '.format(self.name))
+        card = self.chooseCard()
+        return description, card
+
+
+    def chooseCard(self):
+        while True:
+            card = int(input('{}, choose one of your card ({}): '.format(self.name, self.hand)))
+            if self.getCard(card):
+                return card
+
+
     def printState(self):
-        print(self.name, self.hand)
+        print(self.name)
+        print('  hand:', self.hand)
+        print('  score:', self.score)
+        print()
+
+
+    def addScore(self, score):
+        self.score += score
 
 
 class Deck:
@@ -43,16 +75,93 @@ class Deck:
         return result
 
 
+class Gallery:
+
+    def __init__(self, host, players, playersByName):
+        self.host = host
+        self.players = players
+        self.playersByName = playersByName
+        self.cards = {}
+        self.descriprion, self.hostcard = host.createAssociation()
+        self.cards[host.name] = self.hostcard
+
+
+    def choosePlayer(self):
+        name = input('Choose player who pushing card (name or number, 0 - end of making): ')
+        if name == '' or name == '0':
+            return 0
+        if name in self.playersByName:
+            return self.playersByName[name]
+        if not name.isdigit():
+            return
+        index = int(name) - 1
+        if index >= len(self.players):
+            return
+        return self.players[index]
+
+
+    def debtors(self):
+        return set(self.playersByName.keys()).difference(set(self.cards.keys()))
+
+
+    def getCards(self):
+        return self.cards.values()
+
+
+    def pushCards(self):
+        while True:
+            debtors = self.debtors()
+            print('Gallery:', self.cards)
+            print('Debtors:', debtors)
+            player = self.choosePlayer()
+            if player is None:
+                print('Unknown player')
+                continue
+            if player == 0:                
+                if len(debtors) == 0 or input('Are you sure (y/n)? ').lower() == 'y':
+                    break
+                continue
+            print('You have chosen', player.name)
+            card = self.cards.get(player.name)
+            if card is not None:
+                print('Player {} have already pushed card {}'.format(player.name, card))
+                if input('Are you sure to change player card in gallery (y/n)? ').lower() != 'y':
+                    continue
+                player.takeBackCard(card)
+            card = self.cards[player.name] = player.chooseCard()
+            if player == self.host:
+                self.hostcard = card
+
+
+    # self.owners = dict(map(lambda item: (item[1], item[0]), self.cards.items()))
+
+
 class Game:
        
     def __init__(self):
         self.file = open('game.txt', 'r', encoding='utf8')
         self.numPlayers, self.numTotalCards, self.numCardsInHand = map(int, self.file.readline().split())
         assert(self.numPlayers * (self.numCardsInHand + self.numPlayers - 1) <= self.numTotalCards)
-
         self.createPlayers()
         self.createDeck()
-        self.giveCardsToEachPlayer(5)
+
+
+    def start(self):
+        random.shuffle(self.players)
+        self.giveCardsToEachPlayer(self.numCardsInHand)
+        self.round = 0
+        for self.host in self.players:
+            self.round += 1
+            self.printState()
+            currentGallery = Gallery(self.host, self.players, self.playersByName)
+            currentGallery.pushCards()
+            galleryCards = set(currentGallery.getCards())
+            print(sorted(galleryCards))
+            break
+            """
+            self.voting()
+            self.calcResult()
+            """
 
 
     def createPlayers(self):
@@ -63,7 +172,6 @@ class Game:
             player = Player(name)
             self.players.append(player)
             self.playersByName[name] = player
-        random.shuffle(self.players)
 
 
     def createDeck(self):
@@ -84,10 +192,13 @@ class Game:
 
 
     def printState(self):
+        print('Round {}\n'.format(self.round))
+        print('Host: {}\n'.format(self.host.name))
         for player in self.players:
             player.printState()
         print('Deck:', self.deck.cards)
+        print()
 
 
 game = Game()
-game.printState()
+game.start()
